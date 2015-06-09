65eb37d7973f41b809e3d64e0e2fdf3f591b4ec2
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-06-09 21:49:50 +0400

    Created simple RecyclerView

--- app/src/main/java/com/jiahaoliuliu/simplerecyclerviewwithcards/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/simplerecyclerviewwithcards/MainActivity.java
@@ -2,12 +2,36 @@ package com.jiahaoliuliu.simplerecyclerviewwithcards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] myDataset = new String[]{"a", "b", "c", "d", "e",
+            "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"};
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Link the elements
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // This this setting to improve the performance if you know that changes
+        // in content do not change the layout sie of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // Use a linearLayoutManager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Specify an adapter
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
